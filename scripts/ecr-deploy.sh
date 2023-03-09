e6c06b958969ed3fc44e10c6eee12321fb31f51e
Xavier <xavierh93@hotmail.com>
2023-03-09 14:30:39 -0500

    add login to deploy script

--- scripts/ecr-deploy.sh
+++ scripts/ecr-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/i7c1y0j6
 docker build -t xsj-newsletter-microservice .
 docker tag xsj-newsletter-microservice:latest public.ecr.aws/i7c1y0j6/xsj-newsletter-microservice:latest
 docker push public.ecr.aws/i7c1y0j6/xsj-newsletter-microservice:latest
