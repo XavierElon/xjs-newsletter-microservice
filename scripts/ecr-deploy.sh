e4f8f731d5cea8e85ab819e8b48dc8f4de909c76
Xavier <xavierh93@hotmail.com>
2023-03-10 10:38:31 -0500

    update script

--- scripts/ecr-deploy.sh
+++ scripts/ecr-deploy.sh
@@ -3,3 +3,4 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker build -t xsj-newsletter-microservice .
 docker tag xsj-newsletter-microservice:latest 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-newsletter-microservice:latest
 docker push 275136276893.dkr.ecr.us-west-2.amazonaws.com/xsj-newsletter-microservice:latest
+#aws ecs update-service --cluster xsj-newsletter-microservice-cluster --service xsj-newsletter-microservice-service --force-new-deployment --region us-west-2
