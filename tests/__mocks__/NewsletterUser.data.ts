a975c55f7833eac9a672dfb745cd47bbc2eb62cd
Xavier <xavierh93@hotmail.com>
2023-03-13 19:04:19 -0400

    setup tests

---
+++ tests/__mocks__/NewsletterUser.data.ts
@@ -0,0 +1,12 @@
+import { NewsletterUser } from "../../src/models/newsletterUser.model"
+
+export const NewsletterUserMocks =[
+    {
+        email: 'xavier@test.com',
+        subscribed: true
+    },
+    {
+        email: 'achilles@gmail.com',
+        subscribed: false
+    }
+]
--- tests/routes/newsletterUserRoutes.spec.ts
+++ tests/routes/newsletterUserRoutes.spec.ts
@@ -4,8 +4,10 @@ import request from 'supertest'
 import sinon from 'sinon'
 import dotenv from 'dotenv'
 import express, { Express } from 'express'
+import { connectToDatabase } from '../../src/connections/mongodb'
 import { newsletterRouter } from '../../src/routes/newsletterUser.routes'
 import { NewsletterUser } from '../../src/models/newsletterUser.model'
+import { NewsletterUserMocks } from '../__mocks__/NewsletterUser.data'
 
 dotenv.config()
 
@@ -14,5 +16,20 @@ app.use(express.json())
 app.use('/', newsletterRouter)
 
 describe('Newsletter Routes', () => {
-    console.log(process.env.HOST)
+    console.log(NewsletterUserMocks)
+    const testDbUri: string = process.env.TEST_DB_URI!
+
+    before(async () => {
+
+        await connectToDatabase(testDbUri as string)
+    })
+
+    after(async () => {
+        console.log('after')
+        await mongoose.disconnect()
+    })
+
+    it('test', () => {
+        console.log('testing')
+    })
 })
