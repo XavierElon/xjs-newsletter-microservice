90188061655d0a539a4e1012b8cdc34889a87c60
Xavier <xavierh93@hotmail.com>
2023-03-14 10:14:04 -0400

    3 more tests working

--- tests/routes/newsletterUserRoutes.spec.ts
+++ tests/routes/newsletterUserRoutes.spec.ts
@@ -17,7 +17,8 @@ app.use('/', newsletterRouter)
 
 describe('Newsletter Routes', () => {
     let userId
-    let userEmail = 'xavier@test.com4'
+    let userEmail = 'xavier@test.com'
+    const badEmail = 'achillesflocka@gmail.com'
     console.log(NewsletterUserMocks)
     const testDbUri: string = process.env.TEST_DB_URI!
 
@@ -46,17 +47,31 @@ describe('Newsletter Routes', () => {
         expect(res.body.users.length).to.equal(2)
     })
 
-    it('should find a newletter user by email', async () => {
-        const res  = await request(app).get('/newsletter/xavier@test.com')
-        console.log(res)
+    it('should find a newletter user by email with 201 status code', async () => {
+        const res  = await request(app).get(`/newsletter/${userEmail}`)
+        expect(res.status).to.equal(201)
     })
 
-    it('should update a newsletter user by email', () => {
-
+    it('should update a newsletter user by email with 201', async () => {
+        const newUser = {
+            email: 'elonmusk@gmail.com'
+        }
+        const res = await request(app).patch(`/newsletter/${userEmail}`).send(newUser)
+        expect(res.status).to.equal(200)
+        expect(res.body.result.email).to.equal(newUser.email)
     })
 
-    it('should update newsletter user by id', () => {
-
+    it('should update newsletter user by id with 200 status code and subscribed should be changed to false', async () => {
+        const newUser = {
+            email: 'elonmusk@gmail.com',
+            subscribed: false
+        }
+        const res = await request(app).get('/newsletter')
+        userId = res.body.users[0]._id
+        const response = await request(app).patch(`/newsletter/update/${userId}`).send(newUser)
+        console.log(response.body)
+        expect(response.status).to.equal(200)
+        expect(response.body.result.subscribed).to.equal(false)
     })
 
     it('should delete a user by email', () => {
