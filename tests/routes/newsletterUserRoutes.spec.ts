4c349f8ae3f8871e7c94f4d37cd20dfab8809a72
Xavier <xavierh93@hotmail.com>
2023-03-14 10:17:41 -0400

    add 2 more tests

--- tests/routes/newsletterUserRoutes.spec.ts
+++ tests/routes/newsletterUserRoutes.spec.ts
@@ -19,7 +19,6 @@ describe('Newsletter Routes', () => {
     let userId
     let userEmail = 'xavier@test.com'
     const badEmail = 'achillesflocka@gmail.com'
-    console.log(NewsletterUserMocks)
     const testDbUri: string = process.env.TEST_DB_URI!
 
     before(async () => {
@@ -59,6 +58,7 @@ describe('Newsletter Routes', () => {
         const res = await request(app).patch(`/newsletter/${userEmail}`).send(newUser)
         expect(res.status).to.equal(200)
         expect(res.body.result.email).to.equal(newUser.email)
+        expect(res.body.result.subscribed).to.equal(true)
     })
 
     it('should update newsletter user by id with 200 status code and subscribed should be changed to false', async () => {
@@ -69,12 +69,18 @@ describe('Newsletter Routes', () => {
         const res = await request(app).get('/newsletter')
         userId = res.body.users[0]._id
         const response = await request(app).patch(`/newsletter/update/${userId}`).send(newUser)
-        console.log(response.body)
         expect(response.status).to.equal(200)
         expect(response.body.result.subscribed).to.equal(false)
     })
 
-    it('should delete a user by email', () => {
+    it('should delete a user by email with 201 status code', async () => {
+        const email: string = 'elonmusk@gmail.com'
+        const res = await request(app).delete(`/newsletter/${email}`)
+        expect(res.status).to.equal(201)
+    })
 
+    it('should be only one user remaining', async () => {
+        const res = await request(app).get('/newsletter')
+        expect(res.body.users.length).to.equal(1)
     })
 })
