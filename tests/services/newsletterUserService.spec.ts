7f1cadf5dec0b3ea794f61242571bf86e8488176
Xavier <xavierh93@hotmail.com>
2023-03-15 09:51:10 -0400

    initial setup/outline for service tests

--- tests/services/newsletterUserService.spec.ts
+++ tests/services/newsletterUserService.spec.ts
@@ -13,4 +13,54 @@ import { NewsletterUserMocks } from '../__mocks__/NewsletterUser.data'
 
 dotenv.config()
 
-const
+describe('Newsletter User Services Suite', () => {
+    let userId: ObjectId
+    let userEmail: string = 'xavier@test.com'
+    const newEmail: string = 'elonmusk@gmail.com'
+    const nonExistentEmail: string  = 'achillesflocka@gmail.com'
+    const existingEmail: string = 'achilles@gmail.com'
+    const invalidEmail: string = 'achillesflocka@gmail'
+    const testDbUri: string = process.env.TEST_DB_URI!
+
+    before(async () => {
+        await connectToDatabase(testDbUri as string)
+    })
+
+    after(async () => {
+        // Empty database
+        NewsletterUser.deleteMany({}, () => {
+            console.log('NEWSLETTER USERS DELETED')
+        })
+        await mongoose.disconnect()
+    })
+
+    it('should return all newsletter users (2)', async () => {
+
+    })
+
+    it('should return a single user by email', async () => {
+
+    })
+
+    it('should create a newsletter user', async () => {
+
+    })
+
+    it('should find a newsletter user by id', async () => {
+
+    })
+
+    it('should update a newsletter user by email', async () => {
+
+    })
+
+    it('should update a newsletter user by id', async () => {
+
+    })
+
+    it('should delete a newsletter user by email', async () => {
+        
+    })
+
+
+})
